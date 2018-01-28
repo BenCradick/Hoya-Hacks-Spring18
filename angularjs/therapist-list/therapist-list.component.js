bfe762ff33cc7991f86654f87fd2233c682d3532
spolisar <spolisar@gmail.com>
2018-01-28 02:32:40 -0500

    angular stuff, not sure how useful

---
+++ angularjs/therapist-list/therapist-list.component.js
@@ -0,0 +1,6 @@
+angular.module('therapistList').component('therapistList', {
+  //this.therapists = [ /*stuff here*/]
+  this.therapists = new Array(/* length of therapists json */)
+  //loop through therapists json
+  this.therapists[i] = {/* ith therapist */, rank:-1}
+});
---
+++ angularjs/user/user.component.js
@@ -0,0 +1,9 @@
+angular.module('user').component('user', {
+  this.name = 'placeholder_name';
+  this.age = -1;
+  this.gender = 'placeholder_gender';
+  this.sexuality = 'placeholder_sexuality';
+  this.counselorGenderPreference = 'placeholder_counselorGenderPreference';
+  this.areaOfNeed = 'placeholder_areaOfNeed';
+
+});
